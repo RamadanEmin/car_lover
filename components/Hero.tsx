f529b9140b27065f8fbea2e314b4cf68aa2009c8
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-05 19:11:58 +0300

    Added 'Custom button' component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,17 @@
 
 import Image from "next/image";
 
+import CustomButton from "./CustomButton";
+
 const Hero = () => {
+    const handleScroll = () => {
+        const nextSection = document.getElementById("discover");
+    
+        if (nextSection) {
+          nextSection.scrollIntoView({ behavior: "smooth" });
+        }
+      };
+      
     return (
         <div className="hero">
             <div className="flex-1 pt-36 padding-x">
@@ -13,6 +23,12 @@ const Hero = () => {
                 <p className="hero__subtitle">
                     Streamline your car rental experience with our effortless booking process.
                 </p>
+
+                <CustomButton
+                    title="Explore Cars"
+                    containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                    handleClick={handleScroll}
+                />
             </div>
             <div className="hero__image-container">
                 <div className="hero__image">
