446501afa859488aeb89e7fb503e7ded4eac95af
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-07 14:39:29 +0300

    Added SearchBar component

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,13 @@
 import Hero from './Hero';
 import CustomButton from './CustomButton';
 import Navbar from './Navbar';
+import SearchBar from './SearchBar';
 import Footer from './Footer';
 
 export {
     Hero,
     CustomButton,
     Navbar,
+    SearchBar,
     Footer
 };
