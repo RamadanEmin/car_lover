850ac41fefdb7331d4489f643aaf9410fb65b072
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-07 09:31:16 +0300

    Added Navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import { Navbar } from '@/components';
 
 export const metadata = {
     title: 'Car Lover',
@@ -13,6 +14,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className="relative">
+                <Navbar />
                 {children}
             </body>
         </html>
