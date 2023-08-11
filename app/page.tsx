5109e96a3e378fe501e846bc5a9f9313e019617a
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-11 19:16:32 +0300

    Update Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,53 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
 
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CustomFilter, Hero, SearchBar } from '@/components';
 import { fuels, yearsOfProduction } from '@/constants';
+import { fetchCars } from '@/utils';
 
 export default function Home() {
+    const [allCars, setAllCars] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     const [manufacturer, setManufacturer] = useState('');
     const [model, setModel] = useState('');
 
+    const [fuel, setFuel] = useState('');
+    const [year, setYear] = useState(2022);
+
+    const [limit, setLimit] = useState(10);
+
+    const getCars = async () => {
+        setLoading(true);
+
+        try {
+            const result = await fetchCars({
+                manufacturer: manufacturer || '',
+                year: year || 2022,
+                fuel: fuel || '',
+                limit: limit || 10,
+                model: model || ''
+            });
+
+            setAllCars(result);
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getCars();
+    }, [fuel, year, limit, manufacturer, model]);
+
     return (
         <main className="overflow-hidden">
-             <Hero />
+            <Hero />
 
-             <div className="mt-12 padding-x padding-y max-width" id="discover">
+            <div className="mt-12 padding-x padding-y max-width" id="discover">
                 <div className="home__text-container">
                     <h1 className="text-4xl font-extrabold">Car Catalog</h1>
                     <p>Explore the cars you might like</p>
@@ -27,6 +61,34 @@ export default function Home() {
                         <CustomFilter title="year" options={yearsOfProduction} setFilter={setYear} />
                     </div>
                 </div>
+
+                {allCars.length > 0 ? (
+                    <section>
+                        <div className="home__cars-wrapper">
+                            {allCars?.map((car) => (
+                                <CarCard />
+                            ))}
+                        </div>
+
+                        {loading && (
+                            <div className="mt-16 w-full flex-center">
+                                <Image 
+                                src="/loader.svg"
+                                alt="loader"
+                                width={50}
+                                height={50}
+                                className="object-contain"
+                                />
+                            </div>
+                        )}
+
+                    </section>
+                ) : (
+                    <div className="home__error-container">
+                        <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+                        <p>{allCars?.message}</p>
+                    </div>
+                )}
             </div>
         </main>
     );
