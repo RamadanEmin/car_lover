a23718646a58ebb0115f4c4a3ae3ac6fd5483f78
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-11 20:40:58 +0300

    Added 'Generate car Image url' function

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,4 @@
-import { FilterProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
 export async function fetchCars(filters: FilterProps) {
     const { manufacturer, year, model, limit, fuel } = filters;
@@ -29,3 +29,17 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
     return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+    const { make, model, year } = car;
+
+    url.searchParams.append('customer', 'desuchitraswaincompany');
+    url.searchParams.append('make', make);
+    url.searchParams.append('modelFamily', model.split(" ")[0]);
+    url.searchParams.append('zoomType', 'fullscreen');
+    url.searchParams.append('modelYear', `${year}`);
+    url.searchParams.append('angle', `${angle}`);
+
+    return `${url}`;
+}
