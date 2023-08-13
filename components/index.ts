446595116215090f1176bad3fed6fe468a024043
Ramadan Emin <ramadan.a.emin@gmail.com>
2023-08-13 09:09:36 +0300

    Added 'Car details' component

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,7 @@ import SearchBar from './SearchBar';
 import CustomFilter from './CustomFilter';
 import SearchManufacturer from './SearchManufacturer';
 import CarCard from './CarCard';
+import CarDetails from './CarDetails';
 import Footer from './Footer';
 
 export {
@@ -15,5 +16,6 @@ export {
     CustomFilter,
     SearchManufacturer,
     CarCard,
+    CarDetails,
     Footer
 };
